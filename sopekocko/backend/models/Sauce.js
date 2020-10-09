f9d7b610274d9e4cc54a91a0eb877fe8ccbf76d6
Moustapha Touré <moostoure@gmail.com>
2020-10-09 20:38:37 +0200

    commit sopekocko backend

---
+++ sopekocko/backend/models/Sauce.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema(
+ {
+    userId: {type: String , required: true},
+    name: { type: String , required: true},
+    manufacturer: { type: String , required: true},
+    description: { type: String , required: true},
+    mainIngredient: { type: String , required: true},
+    imageUrl: { type: String , required: true},
+    heat: { type: Number , required: true , default: 0},
+    likes: { type: Number , required: true , default: 0},
+    dislikes: { type: Number , required: true , default: 0},
+    usersliked: { type: String , required: true , default: []},
+    usersdisliked: { type: String , required: true , default: []},
+ }
+);
+
+module.exports = mongoose.model('Sauce' , sauceSchema);
